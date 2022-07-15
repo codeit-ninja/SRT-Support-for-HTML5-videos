0743a0632fe3e930d5ace4e8a78fba0f8b1beceb
redbullzuiper <richard_mauritz@hotmail.com>
2022-07-15 03:15:39 +0200

    Rename function name

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
-// /**
-//  *         _       _       
-//  *        (_)     (_)      
-//  *   _ __  _ _ __  _  __ _     
-//  *  | '_ \| | '_ \| |/ _` |    
-//  *  | | | | | | | | | (_| |    
-//  *  |_| |_|_|_| |_| |\__,_|    
-//  *               _/ |    
-//  *              |__/    
-//  */
-// import { hmsToSeconds, toVttCue } from './helpers';
-// import { transformSrtTracks } from './src/transformer';
+/**
+ *         _       _       
+ *        (_)     (_)      
+ *   _ __  _ _ __  _  __ _     
+ *  | '_ \| | '_ \| |/ _` |    
+ *  | | | | | | | | | (_| |    
+ *  |_| |_|_|_| |_| |\__,_|    
+ *               _/ |    
+ *              |__/    
+ */
+import { hmsToSeconds, toVttCue } from './helpers';
+import { transformSrtTracks } from './src/transformer';
 
-// export {
-//     hmsToSeconds,
-//     toVttCue,
-//     transformSrtTracks
-// }
-
-import { transform } from 'https://cdn.jsdelivr.net/gh/codeit-ninja/SRT-Support-for-HTML5-videos@dev/dist/main.es.js';
-
-console.log(transform)
+export {
+    hmsToSeconds,
+    toVttCue,
+    transformSrtTracks
+}
